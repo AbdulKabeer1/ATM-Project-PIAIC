3a8a66cc7ebda70973d4c553bb942286a362702f
Abdul Kabeer <abdulkabeer372@gmail.com>
2024-07-21 18:03:02 +0500

    atm project created

---
+++ atm.js
@@ -0,0 +1,61 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+let myPin = 1122;
+let myBalance = 30000;
+let Mypin = await inquirer.prompt([
+    {
+        name: "Pin",
+        type: "number",
+        message: "Please ENTER your PIN",
+    },
+]);
+if (Mypin.Pin === myPin) {
+    console.log("Your PIN is Correct");
+    let userNeed = await inquirer.prompt([
+        {
+            name: "requirement",
+            type: "list",
+            message: "Please Select one below",
+            choices: ["Cash Withdrawal", "Fast Cash", "Check Balance"],
+        },
+    ]);
+    if (userNeed.requirement === "Cash Withdrawal") {
+        let amount = await inquirer.prompt([
+            {
+                name: "userInput",
+                type: "number",
+                message: "Please ENTER your Amount",
+            },
+        ]);
+        if (amount.userInput > myBalance) {
+            console.log("Your have Insufficient Balance");
+        }
+        else {
+            myBalance = myBalance - amount.userInput;
+            console.log(`Your Balance is ${myBalance}`);
+        }
+    }
+    else if (userNeed.requirement === "Fast Cash") {
+        let fastWithdraw = await inquirer.prompt([
+            {
+                name: "Fastans",
+                type: "list",
+                message: "Please Select your amount",
+                choices: ["500", "1000", "2000", "5000", "10000"],
+            },
+        ]);
+        if (fastWithdraw.Fastans > myBalance) {
+            console.log("Your have Insufficient Balance");
+        }
+        else {
+            myBalance = myBalance - fastWithdraw.Fastans;
+            console.log(`Your Balance is ${myBalance}`);
+        }
+    }
+    else if (userNeed.requirement === "Check Balance") {
+        console.log(`Your Balance is ${myBalance}`);
+    }
+}
+else {
+    console.log("Your PIN is Incorrect");
+}
